a2c0c0f3747d6e97aad3d251f6f7af653d5736c6
DarkyZShadow <s.soulie@hotmail.fr>
2017-03-14 17:09:56 +0100

    Added some things

--- Client/app.js
+++ Client/app.js
@@ -1,19 +1,41 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+let express = require('express');
+let app = express();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+
+let clients = Array();
 
 io.on('connection', function(socket){
-    console.log("connection");
-    io.on('event', function(data){
-        console.log("event");
-    });
-    io.on('disconnect', function(){
-        console.log("disconnect");
-    });
+	if (!containsSocket(clients, socket))
+	{
+		console.log("New client");
+		
+		socket.on('event', function(data){
+			console.log("event");
+		});
+		socket.on('disconnect', function(){
+			console.log("disconnect");
+			clients.splice(clients.indexOf(5), 1);
+		});
+		clients.push(socket);
+	}
 });
 
-app.use ('/', express.static(__dirname));
+app.get('/',function(req, res){
+	res.sendFile(__dirname  + '/static/' + 'index.html');
+});
+app.use('/', express.static(__dirname + '/static'));
+
 server.listen(8080, function(){
     console.log('listening on *:80');
 });
+
+function containsSocket(arr, sock)
+{
+	var i = arr.length;
+	
+	while (i--)
+		if (arr[i].address === sock.address && arr[i].port === sock.port)
+           return true;
+    return false;
+}
---
+++ Client/static/js/index.js
@@ -0,0 +1,112 @@
+$(function ()
+{
+	$('form').submit(function (event) {
+		var input = $("#search_input").val()
+		
+		if (input.length > 0)
+		{
+			
+			
+			$("#search").removeClass("open");
+			$("#search").addClass("close");
+			
+			//
+			// REQUETE SUR L'API
+			//
+			var result = {
+				datas: {
+					SIREN: 123456789,
+					SIRET: "1234567980123",
+					NIC: 123
+				},
+				missing: [
+					'emails', 'address'
+				]
+			};
+			
+			
+			
+			
+			var socket = io();
+			socket.emit('', input);
+		}
+		
+		event.preventDefault();
+        return false;
+		
+        /*var input = $("#search_input").val();
+        if (input.length > 0) {
+            var object = {
+                "value": input
+            };
+            $.ajax({
+                type: "POST",
+                data: JSON.stringify(object),
+                url: "172.16.1.198/api/POST",
+                contentType: "application/json"
+            })
+            $('#search').removeClass('open');
+        }
+        event.preventDefault();
+        return false;*/
+    });
+
+	$('a[href="#search"]').on('click', function (event) {
+        event.preventDefault();
+        $('#search').addClass('open');
+        $('#search > form > input[type="search"]').focus();
+    });
+	
+    $('#search, #search button.close').on('click keyup', function (event) {
+        if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+            $(this).removeClass('open');
+        }
+    });
+});
+
+$(document).ready(function(){
+    $('#table_members').DataTable();
+});
+
+function search()
+{
+	var val = $('#search_input').value;
+	alert(JSON.stringify($('#search_input')));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
