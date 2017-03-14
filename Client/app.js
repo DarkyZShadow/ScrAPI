8178c43c038ca85c841b52123a304d2ce7b33a0b
root <root@ubuntu-server.etna.local>
2017-03-14 14:23:38 +0100

    add node client

---
+++ Client/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+io.on('connection', function(socket){
+    console.log("connection");
+    io.on('event', function(data){
+        console.log("event");
+    });
+    io.on('disconnect', function(){
+        console.log("disconnect");
+    });
+});
+
+app.use ('/', express.static(__dirname));
+server.listen(80, function(){
+    console.log('listening on *:80');
+});
