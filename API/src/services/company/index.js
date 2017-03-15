83d6be94bc77ba93dc56afa002c10bf8dbc20176
root <root@ubuntu-server.etna.local>
2017-03-15 09:58:40 +0100

    /company return object

--- API/src/services/company/index.js
+++ API/src/services/company/index.js
@@ -19,6 +19,20 @@ var schema = new db.mongoose.Schema({
 schema.set('collection', 'raw_datas');
 var Model = mongoose.model('datas', schema);
 
+let every_properties = {
+	SIREN: "SIREN",
+	SIRET: "SIRET",
+	NIC: "NIC",
+	name: "nomen_long",
+	address: "address",
+	members: "members",
+	type: "libnj",
+	desc: "libape",
+	size: "libtef",
+	category: "categorie",
+	date: "dcr"
+};
+
 module.exports = {
     POST: function (req, res)
 				{
@@ -28,7 +42,21 @@ module.exports = {
                 status: 500
             	});
         		} else {
-            	res.jsonp(models);
+							let object = JSON.parse(JSON.stringify(models[0]));
+							var send = {
+								data: {},
+								missing: []
+							};
+							for(let i = 0; i < Object.keys(every_properties).length; i++) {
+								let key = Object.keys(every_properties)[i];
+								let value = every_properties[key];
+								if (object[value]) {
+									send.data[key] = object[value];
+								} else {
+									send.missing.push(key);
+								}
+							}
+            	res.jsonp(send);
         		}
     			});
 				}
